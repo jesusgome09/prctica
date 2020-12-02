13f027f0a5a6641eb3eb36df22e61089c4b99f28
= <=>
2020-12-02 17:41:48 -0500

    login

---
+++ clases.js
@@ -0,0 +1,8 @@
+function go(){
+    if(document.forms.password.value=="jesus" && document.forms.login.value=="gomez"){
+        document.forms.submit();
+    }
+    else{
+        alert("pareses pendejo");
+    }
+}
