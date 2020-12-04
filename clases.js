9727dcc18114b228e440d972d00f4c0827402e97
= <=>
2020-12-03 23:30:42 -0500

    reparar

--- clases.js
+++
@@ -1,8 +0,0 @@
-function go(){
-    if(document.forms.password.value=="jesus" && document.forms.login.value=="gomez"){
-        document.forms.submit();
-    }
-    else{
-        alert("pareses pendejo");
-    }
-}
